39b4056c5b5326fa3872450c56e8624d21b5d30f
Kelvin Azaria <kelvin.azaria.billy@gmail.com>
2019-07-23 15:07:00 +0700

    fixed logout issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :campaign_complaints
+  has_many :campaign_complaints, dependent: :destroy
   has_many :campaigns, dependent: :destroy
   
   # Include default devise modules. Others available are:

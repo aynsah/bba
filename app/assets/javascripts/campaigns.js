15471af8de7107cff5855fe1dca8cf92316d585f
Nathieq Sah <nathieqs16@gmail.com>
2019-07-30 14:30:54 +0700

    Fix Paginate

--- app/assets/javascripts/campaigns.js
+++ app/assets/javascripts/campaigns.js
@@ -3,11 +3,10 @@
 $(document).ready(function () {
   if ($('.pagination').length && $('#search-output-table').length) {
     $(window).scroll(function() {
-      var url = $('.pagination .next_page').attr('href');
+      var url = $('.pagination .page-item:last a').attr('href');
       if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 60) {
-        alert("Hello");
-        $('.pagination').text("Loading more Campaigns");
         $.getScript(url);
+        $('.pagination').text("Loading more Campaigns");
       }
     });
     $(window).scroll();

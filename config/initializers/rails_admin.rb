0082018f079e6db8c2b40bbe9404d5ff966d661c
Kelvin Azaria <kelvin.azaria.billy@gmail.com>
2019-08-09 12:51:35 +0700

    Changed the position of Cek resi menu

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -26,7 +26,7 @@ RailsAdmin.config do |config|
   # config.show_gravatar = true
   config.navigation_static_links = {
     'Google' => 'http://www.google.com',
-    'Home' => 'http://28a14d7b.ngrok.io/'
+    'Home' => 'http://localhost:3000'
   }
   config.navigation_static_label = "Custom Links"
 

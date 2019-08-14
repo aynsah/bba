5a5e98ad4b77479a71c6e43b4a92ab777053ccf6
Nathieq Sah <nathieqs16@gmail.com>
2019-08-14 10:27:47 +0700

    test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 	def index
 		@campaigns = Campaign.where(:status => "approved").last(3)
-		@total = Campaign.count
+		@total = Campaign.where(:status => "approved").count
 	end
 end

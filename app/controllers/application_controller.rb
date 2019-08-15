75e7fdfb1f635e95fe88fb74137f2f21c1c4bcac
Nathieq Sah <nathieqs16@gmail.com>
2019-08-15 09:19:50 +0700

    add subscribed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,6 +25,6 @@ class ApplicationController < ActionController::Base
 
       def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :subscribed) }
-        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :current_password, :name, :phone, :address, :bio, :photo) }
+        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :current_password, :name, :phone, :address, :bio, :photo, :subscribed) }
       end
 end
